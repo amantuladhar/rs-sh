485d32bd8295caea1cb73c81fb3d042ccc2dfa06
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-06-04 13:22:27 -0500

    Loop is REPL????

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,18 @@ mod utils;
 fn main() {
     utils::logger::setup().expect("Unable to setup logger");
 
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    println!("{}: command not found", input.trim());
-    io::stdout()
-        .flush()
-        .context(fdbg!("Failed to flush stdout"))
-        .unwrap();
+        println!("{}: command not found", input.trim());
+        io::stdout()
+            .flush()
+            .context(fdbg!("Failed to flush stdout"))
+            .unwrap();
+    }
 }
