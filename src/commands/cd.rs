9f23c82ade596d0490a538805fc0e255620abf89
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-06-05 12:51:07 -0500

    cd home

--- src/commands/cd.rs
+++ src/commands/cd.rs
@@ -1,10 +1,14 @@
 use super::{Command, CommandError};
 
 pub(crate) fn cd_cmd(args: &str) {
-    use std::env::set_current_dir;
-    match set_current_dir(args) {
+    use std::env::{set_current_dir, var};
+    let args = match args {
+        "~" => var("HOME").unwrap_or("/".into()),
+        _ => args.into(),
+    };
+    match set_current_dir(&args) {
         Ok(_) => (),
-        Err(_) => eprintln!("cd: {}: No such file or directory", args),
+        Err(_) => eprintln!("cd: {}: No such file or directory", &args),
     }
 }
 
